2add3580f4b30f57011e5173b4bdb2c0e3d43dd1
haseebansari-ui <haseeb.ansari@havmormedia.com>
2026-05-25 17:57:13 +0530

    working on index page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 "use strict"
+
+
 // Mobile menue
 
 const menuBarOpen = document.querySelector(".home-nav-link");
 const menuToggle = document.querySelector(".menue-bar-bx");
+const closeHomeIcone = document.querySelector(".hm-menue-bar-bx")    
 
 menuToggle.addEventListener("click", () => {
     menuBarOpen.classList.toggle("active");
     menuToggle.classList.toggle("active");
 });
+
+closeHomeIcone.addEventListener("click", ()=>{
+    menuBarOpen.classList.toggle("active");
+})
 
 
 const innerMenuToggle = document.querySelector(".inner-menue");
@@ -16,3 +23,13 @@ const innerMenuBarOpen = document.querySelector(".home-nav-link ul .drop-down-bx
 innerMenuToggle.addEventListener("click", () => {
     innerMenuBarOpen.classList.toggle("active");
 });
+
+// FAQ
+
+const accordian = document.querySelectorAll('.contentBx');
+
+for (let i = 0; i < accordian.length; i++) {
+	accordian[i].addEventListener('click', function () {
+		this.classList.toggle('active');
+	});
+}
